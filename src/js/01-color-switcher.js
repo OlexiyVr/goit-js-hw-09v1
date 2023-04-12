b32e147b0de4fabf5de12122e2e73405fdee3558
OlexiyVr <olexiyvr@gmail.com>
2023-04-12 23:44:41 +0300

    third commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+// Імпортування Бібліотеки
+import getRandomHexColor from './RHC';
 
+//Кнопки старт/стоп
+const dataStartRef = document.querySelector('[data-start]');
+const dataStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+// values
+let timerId = null;
+
+// Додавання слухача
+dataStartRef.addEventListener('click', onStart);
+dataStopRef.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(getBgColor, 1000);
+
+  dataStartRef.toggleAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(timerId);
+
+  dataStartRef.removeAttribute('disabled');
+}
+
+
+function getBgColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
