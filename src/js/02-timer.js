1f0703ccf3a4de6044b7d213e4ae92414a108ff6
OlexiyVr <olexiyvr@gmail.com>
2023-04-16 19:37:59 +0300

    WW

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -30,7 +30,6 @@ const options = {
 flatpickr(text, options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
